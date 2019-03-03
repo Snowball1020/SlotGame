333f5580a69b9e192e0e65c1c8cc671564541597
Yuki <adohiito@gmail.com>
2019-03-02 23:43:02 -0500

    Revised sentence and modified amount of money

--- SlotGame.java
+++ SlotGame.java
@@ -6,13 +6,13 @@ public class SlotGame {
 	public static void main(String[] args) {
 
 		//pre condition
-		int YourMoney = 50;
+		int YourMoney = 500;
 
 		while (YourMoney > 0) {
 			int LeftMoney = YourMoney;
 
 			System.out.println("You have: " + YourMoney
-					+ " Dollars left. Press Enter Key to start the game. (1000 dollars for play)");
+					+ " Dollars left. Press enter key to start the game. (100 dollars for play)");
 			Scanner scanner = new Scanner(System.in);
 			scanner.nextLine();
 
@@ -25,31 +25,31 @@ public class SlotGame {
 			// slot col 1
 			for (int i = 0; i < 1; i++) {
 				int answer = rand.nextInt(3);
-				System.out.println("First col is: " + answer);
+				System.out.println("First number is: " + answer);
 				
 				// slot col 2
 				scanner.nextLine();
 				for (int i2 = 0; i2 < 1; i2++) {
 					int answer2 = rand.nextInt(3);
-					System.out.println("Second col is: " + answer2);
+					System.out.println("Second number is: " + answer2);
 					
 					// slot col 3
 					scanner.nextLine();
 					for (int i3 = 0; i3 < 1; i3++) {
 						int answer3 = rand.nextInt(3);
-						System.out.println("Third col is: " + answer3);
+						System.out.println("Third number is: " + answer3);
 						scanner.nextLine();
 
 						if (answer == answer2 && answer2 == answer3) {
-							System.out.println("Congratz!! Got 5000 dollars!!");
+							System.out.println("Congratulations!! You got 5000 dollars!!");
 							YourMoney = YourMoney + 5000;
 						}
 						else if (answer == answer2 || answer2 == answer3 || answer == answer3) {
-							YourMoney = YourMoney - 1000;
+							YourMoney = YourMoney + 50;
 							System.out.println("Almost there!");
 						}
 						else {
-							YourMoney = YourMoney - 1000;
+							YourMoney = YourMoney - 100;
 							System.out.println("You lose. ");
 							System.out.println();
 
@@ -59,7 +59,7 @@ public class SlotGame {
 				}
 			}	
 		}
-		System.out.println("No more money to bet. Game is over");
+		System.out.println("I am sorry , No more money to bet. Game is over");
 	}
 }
 
