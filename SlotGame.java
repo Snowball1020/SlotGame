daeab9bbb11b137c2e604ed4320c9b0045efde15
Yuki <adohiito@gmail.com>
2019-03-02 23:35:07 -0500

    Capitalized

--- SlotGame.java
+++ SlotGame.java
@@ -6,13 +6,13 @@ public class SlotGame {
 	public static void main(String[] args) {
 
 		//pre condition
-		int YourMoney = 5000;
+		int YourMoney = 50;
 
 		while (YourMoney > 0) {
 			int LeftMoney = YourMoney;
 
-			System.out.println("you have: " + YourMoney
-					+ " dollars left. Press Enter Key to start the game. (1000 dollars for play)");
+			System.out.println("You have: " + YourMoney
+					+ " Dollars left. Press Enter Key to start the game. (1000 dollars for play)");
 			Scanner scanner = new Scanner(System.in);
 			scanner.nextLine();
 
@@ -25,19 +25,19 @@ public class SlotGame {
 			// slot col 1
 			for (int i = 0; i < 1; i++) {
 				int answer = rand.nextInt(3);
-				System.out.println("first col is: " + answer);
+				System.out.println("First col is: " + answer);
 				
 				// slot col 2
 				scanner.nextLine();
 				for (int i2 = 0; i2 < 1; i2++) {
 					int answer2 = rand.nextInt(3);
-					System.out.println("second col is: " + answer2);
+					System.out.println("Second col is: " + answer2);
 					
 					// slot col 3
 					scanner.nextLine();
 					for (int i3 = 0; i3 < 1; i3++) {
 						int answer3 = rand.nextInt(3);
-						System.out.println("third col is: " + answer3);
+						System.out.println("Third col is: " + answer3);
 						scanner.nextLine();
 
 						if (answer == answer2 && answer2 == answer3) {
@@ -46,7 +46,7 @@ public class SlotGame {
 						}
 						else if (answer == answer2 || answer2 == answer3 || answer == answer3) {
 							YourMoney = YourMoney - 1000;
-							System.out.println("almost there!");
+							System.out.println("Almost there!");
 						}
 						else {
 							YourMoney = YourMoney - 1000;
